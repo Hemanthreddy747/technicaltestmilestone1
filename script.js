458fa9f763c54271118635e385736ec36de4d521
Hemanth747 <118827928+Hemanthreddy747@users.noreply.github.com>
2023-12-23 15:18:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+ 
+        let currentIndex = 0;
+
+        function showSlide(index) {
+            const slides = document.querySelector('.slides');
+            const totalSlides = document.querySelectorAll('.slide').length;
+            if (index < 0) {
+                currentIndex = totalSlides - 1;
+            } else if (index >= totalSlides) {
+                currentIndex = 0;
+            } else {
+                currentIndex = index;
+            }
+            slides.style.transform = `translateX(${-currentIndex * 100}%)`;
+        }
+
+        function prevSlide() {
+            showSlide(currentIndex - 1);
+        }
+
+        function nextSlide() {
+            showSlide(currentIndex + 1);
+        }
+
+        document.querySelector('.arrow-left').addEventListener('click', prevSlide);
+        document.querySelector('.arrow-right').addEventListener('click', nextSlide);
+
+        // Auto slide change example
+        setInterval(() => {
+            nextSlide();
+        }, 8000); // Change slide every 5 seconds
+
+        // Enable video controls on user interaction
+        document.querySelectorAll('.preview').forEach(video => {
+            video.addEventListener('click', () => {
+                video.controls = true;
+                video.loop = false;
+                video.muted = false;
+                video.play();
+            });
+        });
+   
